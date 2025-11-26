e410e30e4b19b7be5398b6515307ba256a400824
souhail <souhaibadaoui@gmail.com>
2025-11-26 17:01:51 +0100

    merouand

--- src/app.js
+++ src/app.js
@@ -1,30 +1,26 @@
-import "dotenv/config";
 import express from "express";
-import { sequelize } from "./config/database.js";
-import  teamRoutes from "./routes/teamRoutes.js";
-import playerRoutes from "./routes/playerRoutes.js"
-
 const app = express();
+import "dotenv/config"
+import {sequelize} from "./config/database.js"
+// body parser
 app.use(express.json());
+
+// ROUTES
+
+/*
+const teamRoutes = require("./routes/teamRoutes");
+const playerRoutes = require("./routes/playerRoutes");
+const matchRoutes = require("./routes/matchRoutes");
+app.use("/teams", teamRoutes);
+app.use("/players", playerRoutes);
+app.use("/matches", matchRoutes);
+*/
+
+
+sequelize.sync({ alter: true });
 
 const PORT = 3000;
 
-
-sequelize.sync({ alter: true })
-  .then(() => {
-    console.log("Tables created successfully!");
-  })
-  .catch((err) => {
-    console.log("Error creating tables:", err);
-  });
-
-app.use("/teams" , teamRoutes)
-app.use("/player" , playerRoutes)
-
-app.get("/", (req , res) => {
-  res.send("CAN API running...");
-});
-
 app.listen(PORT, () => {
-  console.log("Server running on port 3000");
+  console.log(`Server running on port ${PORT}`);
 });
