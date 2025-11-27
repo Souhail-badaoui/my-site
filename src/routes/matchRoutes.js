ec3144824fa4b6a1f96c54f001df2ca3c741313e
Lakhrouf Mohamed <41682397+SUITSHIMED@users.noreply.github.com>
2025-11-27 11:19:12 +0100

    updating the route

--- src/routes/matchRoutes.js
+++ src/routes/matchRoutes.js
@@ -1,17 +1,17 @@
-// src/routes/matchRoutes.js
+
 import express from 'express';
 import { authenticate, authorizeAdmin } from '../middlewares/authMiddleware.js'; 
 import { createMatch, getMatches, getMatch, updateMatch, deleteMatch } from '../controllers/matchController.js'; 
 
 const router = express.Router();
 
-// Public Routes (No token needed)
+
 router.get('/', getMatches);
 router.get('/:id', getMatch);
 
-// Protected Routes (Requires Admin token)
+
 router.post('/', authenticate, authorizeAdmin, createMatch);
 router.put('/:id', authenticate, authorizeAdmin, updateMatch);
 router.delete('/:id', authenticate, authorizeAdmin, deleteMatch);
 
-export default router; // <-- Exports the Express router instance
+export default router; 
