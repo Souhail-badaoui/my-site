28ba7509106e120d5d0723f14e5fb40dc431ac22
souhail <souhaibadaoui@gmail.com>
2025-11-27 22:37:14 +0100

    TeamsCrud Reverting

--- src/models/Match.js
+++ src/models/Match.js
@@ -1,34 +1,4 @@
 
-<<<<<<< HEAD
-import { DataTypes } from "sequelize";
-import sequelize from "../config/database.js";
-import Team from "./Team.js";
-
-const Match = sequelize.define("Match", {
-  date: { 
-    type: DataTypes.DATE, 
-    allowNull: false 
-  },
-  location: { 
-    type: DataTypes.STRING, 
-    allowNull: false 
-  },
-  scoreA: { 
-    type: DataTypes.INTEGER, 
-    defaultValue: 0 
-  },
-  scoreB: { 
-    type: DataTypes.INTEGER, 
-    defaultValue: 0 
-  },
-});
-
-
-Match.belongsTo(Team, { as: "teamA", foreignKey: "teamAId" });
-Match.belongsTo(Team, { as: "teamB", foreignKey: "teamBId" });
-
-export default Match;
-=======
 
 export default (sequelize, DataTypes) => {
   const Match = sequelize.define(
@@ -62,4 +32,3 @@ export default (sequelize, DataTypes) => {
 
   return Match;
 };
->>>>>>> 0be94a03a7eddcb345deefffec18d9f851a64fb2
